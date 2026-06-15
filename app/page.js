841a6471939503cc62c08c51fe3e9d072e739ebd
zyppayx-lab <zyppayx@gmail.com>
2026-06-15 18:52:54 +0100

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "20px" }}>
-      <h1>OpportunityHub</h1>
-      <p>Discover Opportunities, Scholarships & Side Hustles</p>
-    </main>
-  );
-  }
